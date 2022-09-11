b615ecd817f52371deda9d43b40ad09e87c9d27b
Andrii <nagibator7337@gmail.com>
2022-09-11 19:35:42 +0300

    Added searching for email.

--- src/main/java/com/example/homework/Controllers/UserController.java
+++ src/main/java/com/example/homework/Controllers/UserController.java
@@ -1,13 +1,13 @@
 package com.example.homework.Controllers;
 
+import com.example.homework.Entity.Transporter;
+import com.example.homework.Entity.User;
 import com.example.homework.Services.GoodService;
 import com.example.homework.Services.UserService;
 import com.example.homework.dto.CustomerDto;
 import com.example.homework.dto.UserDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +19,15 @@ public class UserController {
     @GetMapping()
     public List<UserDto> getAll() {
        return userService.getAll();
+    }
+
+    @PostMapping("/register")
+    public void create(@RequestBody User user) {
+        userService.create(user);
+    }
+
+    @GetMapping("/findForEmail")
+    public User findForEmail(@RequestParam String email) {
+        return userService.findByEmail(email);
     }
 }
